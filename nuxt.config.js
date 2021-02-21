3a7853d014edcc95fd13b866d4b71a23b29b073b
miyuki shoji <0428.miyuki.shoji@gmail.com>
2021-02-21 16:18:44 +0900

    🍺date-fns入れた

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,7 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     '@nuxtjs/dotenv',
+    '@nuxtjs/date-fns',
   ],
   /*
    ** Nuxt.js modules
