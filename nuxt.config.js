328b44ef96eccf7a7b61e4959ce847a3e9e6b82e
miyuki shoji <0428.miyuki.shoji@gmail.com>
2021-09-01 20:22:20 +0900

    🚀nuxt.configのメンテ

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,50 +1,82 @@
 const title = 'Nuxt.js project starter template'
 const description = 'This is Nuxt.js project starter template.'
+const uri = 'https://xxxx/'
 const target = 'static' // server or static
 
 export default {
   /*
+   ** --------------------------------------------------
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
+   ** --------------------------------------------------
    */
-  ssr: true,
+  mode: 'universal',
   /*
+   ** --------------------------------------------------
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
+   ** --------------------------------------------------
    */
   target,
   /*
+   ** --------------------------------------------------
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
+   ** --------------------------------------------------
    */
   head: {
     title,
+    titleTemplate: '%s |' + ' ' + title,
     htmlAttrs: {
       lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: description },
+      { hid: 'og:site_name', property: 'og:site_name', content: title },
+      { hid: 'og:type', property: 'og:type', content: 'article' },
+      { hid: 'og:url', property: 'og:url', content: uri },
+      { hid: 'og:title', property: 'og:title', content: title },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: description,
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: uri + 'images/ogp.png',
+      },
+      { name: 'twitter:card', content: 'summary_large_image' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
+   ** --------------------------------------------------
    ** Global CSS and Sass
+   ** --------------------------------------------------
    */
   css: ['ress', '~/assets/scss/base.scss'],
   /*
+   ** --------------------------------------------------
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
+   ** --------------------------------------------------
    */
   plugins: [],
   /*
+   ** --------------------------------------------------
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
+   ** --------------------------------------------------
    */
   components: true,
   /*
+   ** --------------------------------------------------
    ** Nuxt.js dev-modules
+   ** --------------------------------------------------
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
@@ -82,7 +114,9 @@ export default {
     scss: ['~/assets/scss/system.scss'],
   },
   /*
+   ** --------------------------------------------------
    ** Nuxt.js modules
+   ** --------------------------------------------------
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
@@ -100,8 +134,10 @@ export default {
     path: process.cwd(),
   },
   /*
+   ** --------------------------------------------------
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
+   ** --------------------------------------------------
    */
   build: {
     extend(config, ctx) {
