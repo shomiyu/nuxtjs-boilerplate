34f47d0e75472fcc31e36110603d23c9d67bf00b
shomiyu <m1011522@gmail.com>
2022-02-08 23:38:12 +0900

    💄ヘッダーを下部に固定

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="wrapper">
+    <main class="main">
+      <Nuxt />
+    </main>
   </div>
 </template>
+
+<style lang="scss">
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main {
+  flex: 1;
+  overflow-x: hidden;
+}
+</style>
